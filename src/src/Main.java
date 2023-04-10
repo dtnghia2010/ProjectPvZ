f06fd7db7b3c61d9550d2399acb668ce21825ddb
Khoakhoa2812 <khoakhoa2812@gmail.com>
2023-04-10 09:03:02 +0700

    new game loop (make black rectangle moves)

--- src/src/Main.java
+++ src/src/Main.java
@@ -3,11 +3,14 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.add(new World());
+        World newWorld = new World();
+        frame.add(newWorld);
+        newWorld.startThread();
         frame.setTitle("Plants vs Zombies");
         frame.setBounds(127, 0, 1024, 625);
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
     }
 }
--- src/src/World.java
+++ src/src/World.java
@@ -6,17 +6,63 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class World extends JPanel {
+public class World extends JPanel implements Runnable{
     private BufferedImage img;
+    private boolean isWallCollideLeft = true;
+    private boolean isWallCollideRight = false;
     public World() {
-        importImg();
+
+    }
+    Thread gameThread;
+    private Rectangle rectangle = new Rectangle(500,250,100,100);
+    public void startThread(){
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+        double drawInterval = 1000000000/60;
+        double nextDraw = System.nanoTime() + drawInterval;
+        while (gameThread != null){
+            double remainingTime = nextDraw - System.nanoTime();
+            remainingTime = remainingTime/1000000;
+            if(remainingTime < 0){
+                remainingTime = 0;
+            }
+
+            move();
+            try {
+                Thread.sleep((long) remainingTime);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            nextDraw += drawInterval;
+        }
+    }
+    public void move(){
+        if(!isWallCollideRight && isWallCollideLeft){
+            rectangle.setBounds((int)rectangle.getX()+3,(int)rectangle.getY(),(int)rectangle.getWidth(),(int)rectangle.getHeight());
+            if(rectangle.getX() >= 1024 - 110){
+                isWallCollideRight = true;
+                isWallCollideLeft = false;
+            }
+        } else if(!isWallCollideLeft && isWallCollideRight){
+            rectangle.setBounds((int)rectangle.getX()-3,(int)rectangle.getY(),(int)rectangle.getWidth(),(int)rectangle.getHeight());
+            if(rectangle.getX() <= 0){
+                isWallCollideRight = false;
+                isWallCollideLeft = true;
+            }
+        }
+        repaint();
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(img, 0,0,null);
-
+        g.drawImage(img, 0,0,1024,625,null);
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect((int)rectangle.getX(),(int)rectangle.getY(),(int)rectangle.getWidth(),(int)rectangle.getHeight());
     }
     public void importImg() {
         InputStream is = getClass().getResourceAsStream("res/background.jpg");
@@ -26,4 +72,6 @@ public class World extends JPanel {
             e.printStackTrace();
         }
     }
+
+
 }
