5b87c6e423dc2c5a12cf7de69636db3d79109ef1
Khoakhoa2812 <khoakhoa2812@gmail.com>
2023-04-10 09:03:02 +0700

    new game loop (make black rectangle moves)

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,11 +4,14 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.add(new World());
+        World newWorld = new World();
+        frame.add(newWorld);
+        newWorld.startThread();
         frame.setTitle("Plants vs Zombies");
         frame.pack();
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+
     }
 }
