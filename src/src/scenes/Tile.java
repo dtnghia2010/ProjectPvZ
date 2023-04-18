7cbe5c78dd3f1d6c9b453c87d415a6cd4387330b
thuongngo050902 <ngothithuong592002@gmail.com>
2023-04-18 21:24:17 +0700

    prj

--- src/src/scenes/Tile.java
+++ src/src/scenes/Tile.java
@@ -6,23 +6,22 @@ import javax.swing.*;
 public class Tile{
     private static final int ROWS = 5;
     private static final int COLS = 9;
-    private static final int SIZE = 70;
 
 
       public Tile(){}
       public void render(Graphics g) {
         Graphics2D G2D = (Graphics2D) g ;
         G2D.setColor(Color.pink);
-        int up=0;
-        int up1=0;
+        int upX=0;
+        int upY=0;
         for (int i=1; i<=ROWS; i++){
-            for (int j=0; j<=COLS; j++) {
-                Rectangle RTG = new Rectangle(320 + up, 90+up1, 70, 90);
-                up += 80;
+            for (int j=1; j<=COLS; j++) {
+                Rectangle RTG = new Rectangle(500 + upX, 110+upY, 90, 110);
+                upX += 110;
                 G2D.fill(RTG);
             }
-            up1 += 100;
-            up =0;
+            upY += 130;
+            upX =0;
         }
       }
     }
