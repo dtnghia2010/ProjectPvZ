8e91336a3aff95174bfa30bdef18f8b46fbee2b1
Khoakhoa2812 <khoakhoa2812@gmail.com>
2023-04-19 22:08:25 +0700

    bug fixing

--- src/src/Main.java
+++ src/src/Main.java
@@ -4,14 +4,11 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        World newWorld = new World();
-        frame.add(newWorld);
-        newWorld.startThread();
+        frame.add(new World());
         frame.setTitle("Plants vs Zombies");
         frame.pack();
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-
     }
 }
